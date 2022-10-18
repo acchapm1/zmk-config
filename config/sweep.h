1eb1ba4c7e6032198878ab55dd73f7a48337d487
acchapm1 <acchapm1@asu.edu>
2022-10-18 16:51:42 -0700

    rearranged macros and combos

---
+++ config/sweep.h
@@ -0,0 +1,19 @@
+#define xxx &none
+#define ___ &trans
+#define iHOMsel RG(RS(LEFT))
+#define iENDsel RG(RS(RIGHT))
+#define selAll RG(A)
+#define mtRight LC(RIGHT)
+#define mtLeft LC(LEFT)
+#define coPy LG(C)
+#define pasTE LG(V)
+#define raY LA(SPACE)
+#define reDo RG(RS(Z))
+#define unDo RG(Z)
+#define tmuxKey LA(GRAVE)
+#define bT1 BT_SEL 0
+#define bT2 BT_SEL 1
+#define bT3 BT_SEL 2
+#define bTNxt BT_NXT
+#define bTPrv BT_PRV
+#define bTClr BT_CLR
